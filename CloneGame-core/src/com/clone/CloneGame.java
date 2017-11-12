ab98553a58d51f0132b5e22158ece67720fa85e8
theagentd <theagentd@hotmail.com>
2017-11-12 22:18:48 +0100

    Added ragdoll branch and class.

--- CloneGame-core/src/com/clone/CloneGame.java
+++ CloneGame-core/src/com/clone/CloneGame.java
@@ -2,6 +2,7 @@ package com.clone;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -17,51 +18,42 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class CloneGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 	
-	World world;
-	Box2DDebugRenderer debugRenderer;
+	private World world;
+	private Box2DDebugRenderer debugRenderer;
+	
+	private Ragdoll r;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 		
 		world = new World(new Vector2(0, 0), true);
 		debugRenderer = new Box2DDebugRenderer();
-		
-		BodyDef bodyDef = new BodyDef();
-		bodyDef.type = BodyType.DynamicBody;
-		bodyDef.position.set(100, 100);
-		bodyDef.angularVelocity = 1;
-		
-		Body body = world.createBody(bodyDef);
 
-		// Create a circle shape and set its radius to 6
-		CircleShape circle = new CircleShape();
-		circle.setRadius(60f);
-
-		// Create a fixture definition to apply our shape to
-		FixtureDef fixtureDef = new FixtureDef();
-		fixtureDef.shape = circle;
-		fixtureDef.density = 0.5f; 
-		fixtureDef.friction = 0.4f;
-		fixtureDef.restitution = 0.6f; // Make it bounce a little bit
-
-		// Create our fixture and attach it to the body
-		Fixture fixture = body.createFixture(fixtureDef);
+		for(int i = 0; i < 10; i++){
+			r = new Ragdoll(world, 10 + i*2, 20);
+		}
 	}
 
 	private long previousTime = System.nanoTime();
 	
 	@Override
 	public void render () {
+		
+		
+		float force = 0;
+		if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+			force = 20;
+		}
+		if(r.body != null) r.body.applyForceToCenter(+force, 0, true);
+		if(r.head != null) r.head.applyForceToCenter(-force, 0, true);
+		
+		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		Matrix4 matrix = new Matrix4();
-		matrix.setToOrtho2D(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), -1, +1);
+		matrix.setToOrtho2D(0, 0, Gdx.graphics.getWidth()/20f, Gdx.graphics.getHeight()/20f, -1, +1);
 		
 		long time = System.nanoTime();
 		float delta = (time - previousTime) / 1000000000f;
@@ -74,7 +66,6 @@ public class CloneGame extends ApplicationAdapter {
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		
 	}
 }
