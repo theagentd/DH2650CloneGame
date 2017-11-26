9faea980e9804c0946079d86bf3a0d361a6b26f2
theagentd <theagentd@hotmail.com>
2017-11-26 03:18:26 +0100

    Experiments with 3D rendering.

--- CloneGame-core/src/com/clone/CloneGame.java
+++ CloneGame-core/src/com/clone/CloneGame.java
@@ -4,16 +4,39 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 
 public class CloneGame extends ApplicationAdapter {
+	
+	private PerspectiveCamera camera;
+	
+	private ModelBatch modelBatch;
+	private Environment environment;
+	
+	private Model cube;
+	private ModelInstance cubeInstance;
+	
+	
+	
 
 	private World world;
 	private Box2DDebugRenderer debugRenderer;
@@ -27,6 +50,24 @@ public class CloneGame extends ApplicationAdapter {
 
 	@Override
 	public void create() {
+		
+		
+		camera = new PerspectiveCamera(60, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(0, 0, 10);
+		camera.lookAt(0, 0, 0);
+		camera.near = 1;
+		camera.far = 100;
+		
+		modelBatch = new ModelBatch();
+		
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.5f, 0.5f, 0.5f, -1, -1, -1));
+		
+		
+		cube = new ModelBuilder().createBox(1, 1, 1, new Material(ColorAttribute.createDiffuse(1, 0, 0, 0)), Usage.Position | Usage.Normal);
+		cubeInstance = new ModelInstance(cube);
+		
 		world = new World(new Vector2(0, 0), true);
 		world.setGravity(new Vector2(0, -9.81f * 2));
 
@@ -41,10 +82,18 @@ public class CloneGame extends ApplicationAdapter {
 
 	private long previousTime = System.nanoTime();
 
+	float rotation = 0;
 	@Override
 	public void render() {
+		
+		camera.viewportWidth = Gdx.graphics.getWidth();
+		camera.viewportHeight = Gdx.graphics.getHeight();
+		camera.update();
+		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		
+		
 
 		if (player == null || !player.getActive()) {
 			// r = new Ragdoll(world, 55, 210, (float)Math.random());
@@ -117,6 +166,13 @@ public class CloneGame extends ApplicationAdapter {
 		// Gdx.graphics.setTitle("Can jump: " + player.isCanJump());
 
 		debugRenderer.render(world, matrix);
+		
+		rotation += delta * 100;
+		
+		cubeInstance.transform.set(new Vector3(1, -2, 0), new Quaternion().setEulerAngles(rotation, 0, 0));
+		modelBatch.begin(camera);
+		modelBatch.render(cubeInstance, environment);
+		modelBatch.end();
 	}
 
 	@Override
