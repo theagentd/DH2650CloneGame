69d5926d75d7f7f205b06d930e4e6884265e7205
Antonio Morais <antonio.morais@epfl.ch>
2017-11-30 21:11:03 +0100

    UI done, some modifications still to do i would say

--- CloneGame-core/src/com/clone/CloneGame.java
+++ CloneGame-core/src/com/clone/CloneGame.java
@@ -3,6 +3,7 @@ package com.clone;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,7 +12,16 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
+import com.google.gwt.user.client.ui.Widget;
 
 public class CloneGame extends ApplicationAdapter {
 
@@ -20,10 +30,24 @@ public class CloneGame extends ApplicationAdapter {
 	private ContactListener contactListener;
 	private Object currentLevel;
 
+	private Stage stage;
+	private Table table;
+
 	private Ragdoll r;
 	private Player player;
 
 	private int kindOfClone;
+
+	private final static float BUTTONHEIGHT = 50;
+	private final static float BUTTONWIDTH = 300;
+
+	private final static int NORMALCLONE = 0;
+	private final static int BOUNCINGCLONE = 1;
+	private final static int FREEZINGCLONE = 2;
+
+	private boolean noChosenPlayer = false;
+
+	// BUG DOUBLE SPACE
 
 	@Override
 	public void create() {
@@ -33,9 +57,58 @@ public class CloneGame extends ApplicationAdapter {
 		world.setContactListener(contactListener = new ContactListener());
 		debugRenderer = new Box2DDebugRenderer();
 
-		kindOfClone = 0;
+		kindOfClone = NORMALCLONE;
 
 		currentLevel = new Level3(world);
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		table = createTable();
+		stage.addActor(table);
+
+	}
+
+	private Table createTable() {
+		table = new Table();
+		table.setFillParent(true);
+		table.setVisible(false);
+		Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+		table.setSkin(skin);
+		TextButton normal = new TextButton("Normal Clone", skin);
+		TextButton bouncing = new TextButton("Bouncing Clone", skin);
+		TextButton freezing = new TextButton("Freezing Clone", skin);
+		normal.addCaptureListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				kindOfClone = NORMALCLONE;
+				noChosenPlayer = false;
+				table.setVisible(false);
+			};
+		});
+		bouncing.addCaptureListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				kindOfClone = BOUNCINGCLONE;
+				noChosenPlayer = false;
+				table.setVisible(false);
+			};
+		});
+		freezing.addCaptureListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				kindOfClone = FREEZINGCLONE;
+				noChosenPlayer = false;
+				table.setVisible(false);
+			};
+		});
+		table.add("Which clone do you want ?", "default-font", Color.CHARTREUSE).row();
+		table.add(" ").row();
+		table.add(normal).size(BUTTONWIDTH, BUTTONHEIGHT).row();
+		table.add(bouncing).size(BUTTONWIDTH, BUTTONHEIGHT).row();
+		table.add(freezing).size(BUTTONWIDTH, BUTTONHEIGHT).row();
+
+		// table.setDebug(true); // This is optional, but enables debug lines for
+		// tables.
+		return table;
 	}
 
 	private long previousTime = System.nanoTime();
@@ -44,36 +117,41 @@ public class CloneGame extends ApplicationAdapter {
 	public void render() {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		if ((noChosenPlayer && player == null) || ((noChosenPlayer && !player.getActive()))) {
+			table.setVisible(true);
+		}
 
-		if (player == null || !player.getActive()) {
+		if ((!noChosenPlayer && player == null) || ((!noChosenPlayer && !player.getActive()))) {
 			// r = new Ragdoll(world, 55, 210, (float)Math.random());
 			r = new Ragdoll(world, 55, 180, 0.1f);
-
+			// table.setVisible(true);
+			// long previousTime = System.nanoTime();
+			// while(noChosenPlayer) {
+			// table.setVisible(true);
+			// } // loop to let the real player choose the clone he wants
+			
+			noChosenPlayer = true;
 			switch (kindOfClone) {
-			case 0:
+			case NORMALCLONE:
 				player = new Player(r);
 				break;
-			case 1:
+			case BOUNCINGCLONE:
 				player = new BouncingPlayer(r, world);
 				break;
-			case 2:
+			case FREEZINGCLONE:
 				player = new FreezingPlayer(r);
 				break;
 			}
+			
 		}
 
 		// float force = 0;
-		move(player);
-
+		if (player != null) {
+			move(player);
+		}
 		if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
 			player.dispose();
 			r.body.applyAngularImpulse((float) (Math.random() * 2 - 1) * 50, true);
-		} else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)) {
-			kindOfClone = 0;
-		} else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)) {
-			kindOfClone = 1;
-		} else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_3)) {
-			kindOfClone = 2;
 		}
 
 		// if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
@@ -90,8 +168,9 @@ public class CloneGame extends ApplicationAdapter {
 		// float delta = (time - previousTime) / 1000000000f;
 		// previousTime = time;
 		float delta = 1 / 60f;
-
-		player.setJump(false);
+		if (player != null) {
+			player.setJump(false);
+		}
 		int iterations = 1;
 		for (int i = 0; i < iterations; i++) {
 			world.step(delta / iterations, 10, 5);
@@ -99,7 +178,7 @@ public class CloneGame extends ApplicationAdapter {
 				Ragdoll rag = contactListener.deadPlayer.ragdoll;
 				contactListener.deadPlayer.dispose();
 				contactListener.deadPlayer = null;
-				if(contactListener.splash == true){
+				if (contactListener.splash == true) {
 					rag.splash(world);
 					contactListener.splash = false;
 				}
@@ -119,15 +198,16 @@ public class CloneGame extends ApplicationAdapter {
 			} else if (currentLevel instanceof Level3) {
 				currentLevel = new Level4(world);
 			}
-			else if (currentLevel instanceof Level4) {
-				currentLevel = new Level1(world);
-			}
+			 else if (currentLevel instanceof Level4) {
+			 currentLevel = new Level1(world);
+			 }
 			player = null;
 			contactListener.endLevel = false;
 		}
 
 		// Gdx.graphics.setTitle("Can jump: " + player.isCanJump());
-
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 		debugRenderer.render(world, matrix);
 	}
 
@@ -167,90 +247,3 @@ public class CloneGame extends ApplicationAdapter {
 
 	}
 }
-
-/*
- * package com.clone;
- * 
- * import com.badlogic.gdx.ApplicationAdapter; import com.badlogic.gdx.Gdx;
- * import com.badlogic.gdx.Input; import com.badlogic.gdx.graphics.GL20; import
- * com.badlogic.gdx.graphics.Texture; import
- * com.badlogic.gdx.graphics.g2d.SpriteBatch; import
- * com.badlogic.gdx.math.Matrix4; import com.badlogic.gdx.math.Vector2; import
- * com.badlogic.gdx.physics.box2d.Body; import
- * com.badlogic.gdx.physics.box2d.Box2DDebugRenderer; import
- * com.badlogic.gdx.physics.box2d.World; import com.badlogic.gdx.utils.Array;
- * 
- * public class CloneGame extends ApplicationAdapter {
- * 
- * private World world; private Box2DDebugRenderer debugRenderer; private
- * ContactListener contactListener;
- * 
- * private Ragdoll r; private Player player;
- * 
- * @Override public void create() { world = new World(new Vector2(0, 0), true);
- * world.setGravity(new Vector2(0, -9.81f*2));
- * 
- * world.setContactListener(contactListener = new ContactListener());
- * debugRenderer = new Box2DDebugRenderer();
- * 
- * new Level1(world); // new Level2(world); }
- * 
- * private long previousTime = System.nanoTime();
- * 
- * @Override public void render() { Gdx.gl.glClearColor(0, 0, 0, 1);
- * Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
- * 
- * if(player == null || !player.getActive()) { r = new Ragdoll(world, 55, 210,
- * (float)Math.random()); player = new Player(r); }
- * 
- * // float force = 0; move(player);
- * 
- * if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)){ player.dispose();
- * r.body.applyAngularImpulse((float)(Math.random()*2-1) * 50, true); }
- * 
- * // if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){ // force = 20; //
- * player.jump(force); // if(r.body != null) r.body.applyForceToCenter(-force,
- * 0, true); // if(r.head != null) r.head.applyForceToCenter(+force, 0, true);
- * // } float cameraScale = 15f; Matrix4 matrix = new Matrix4();
- * matrix.setToOrtho2D(45, 120, 16*cameraScale, 9*cameraScale, -1, +1);
- * 
- * //long time = System.nanoTime(); //float delta = (time - previousTime) /
- * 1000000000f; //previousTime = time; float delta = 1/60f;
- * 
- * player.setJump(false); int iterations = 1; for(int i = 0; i < iterations;
- * i++) { world.step(delta / iterations, 10, 5); if(contactListener.deadPlayer
- * != null){ contactListener.deadPlayer.dispose(); contactListener.deadPlayer =
- * null; } }
- * 
- * if(contactListener.endLevel) { Array<Body> bodies = new Array<Body>();
- * world.getBodies(bodies); for(Body body : bodies) { world.destroyBody(body); }
- * new Level2(world); player = null; contactListener.endLevel = false; }
- * 
- * 
- * 
- * //Gdx.graphics.setTitle("Can jump: " + player.isCanJump());
- * 
- * debugRenderer.render(world, matrix); }
- * 
- * @Override public void dispose() {
- * 
- * }
- * 
- * private void move(Player player){ int moving = 0; if (player.getActive()) {
- * if (Gdx.input.isKeyPressed(Input.Keys.UP) ||
- * Gdx.input.isKeyPressed(Input.Keys.W)) { player.jump(20); } if
- * (Gdx.input.isKeyPressed(Input.Keys.RIGHT) ||
- * Gdx.input.isKeyPressed(Input.Keys.D)) { player.applyForce2(+10f, 0); moving =
- * 1; } if (Gdx.input.isKeyPressed(Input.Keys.LEFT) ||
- * Gdx.input.isKeyPressed(Input.Keys.A)) { player.applyForce2(-10f, 0); moving =
- * -1; } }
- * 
- * Vector2 v = r.body.getLinearVelocity(); float maxSpeed = 20; if(v.x <
- * -maxSpeed) { v.x = -maxSpeed; } if(v.x > +maxSpeed) { v.x = +maxSpeed; }
- * 
- * if(moving != 0 || (moving < 0 && v.x > 0) || (moving > 0 && v.x < 0)){ v.x *=
- * 0.9f; } r.body.setLinearVelocity(v);
- * 
- * 
- * } }
- */
--- CloneGame-core/src/com/clone/ContactListener.java
+++ CloneGame-core/src/com/clone/ContactListener.java
@@ -34,7 +34,7 @@ public class ContactListener implements com.badlogic.gdx.physics.box2d.ContactLi
 		Object objectB = contact.getFixtureB().getUserData();
 		checkPlayerHit(objectA, objectB);
 		checkEnd(objectA, objectB);
-		//wreckingBallSwitchDirection(objectA, objectB);
+		// wreckingBallSwitchDirection(objectA, objectB);
 		checkBreak(objectA, objectB);
 	}
 
@@ -42,12 +42,11 @@ public class ContactListener implements com.badlogic.gdx.physics.box2d.ContactLi
 	public void endContact(Contact contact) {
 		/*
 		 * Fixture fixtureA = contact.getFixtureA(); Fixture fixtureB =
-		 * contact.getFixtureB(); if (fixtureA == null || fixtureB == null)
-		 * return; if (fixtureA.getUserData() == null || fixtureB.getUserData()
-		 * == null) return; Object objectA =
-		 * contact.getFixtureA().getUserData(); Object objectB =
-		 * contact.getFixtureB().getUserData(); checkBouncingPlayer(objectA,
-		 * objectB); checkCanNotJump(objectA, objectB);
+		 * contact.getFixtureB(); if (fixtureA == null || fixtureB == null) return; if
+		 * (fixtureA.getUserData() == null || fixtureB.getUserData() == null) return;
+		 * Object objectA = contact.getFixtureA().getUserData(); Object objectB =
+		 * contact.getFixtureB().getUserData(); checkBouncingPlayer(objectA, objectB);
+		 * checkCanNotJump(objectA, objectB);
 		 */
 	}
 
@@ -73,18 +72,24 @@ public class ContactListener implements com.badlogic.gdx.physics.box2d.ContactLi
 
 	private void checkPlayerHit(final Object objectA, final Object objectB) {
 		if (objectA instanceof Player || objectB instanceof Player) {
-			if (objectA instanceof Obstacle || (objectA instanceof WreckingBall && ((WreckingBall)objectA).body.getType() != BodyType.StaticBody) || (objectA instanceof Saw && ((Saw)objectA).body.getType() != BodyType.StaticBody)) {
+			if (objectA instanceof Obstacle
+					|| (objectA instanceof WreckingBall
+							&& ((WreckingBall) objectA).body.getType() != BodyType.StaticBody)
+					|| (objectA instanceof Saw && ((Saw) objectA).body.getType() != BodyType.StaticBody)) {
 				if (((Player) objectB).getActive()) {
 					deadPlayer = ((Player) objectB);
 				}
-				if(objectA instanceof Saw && ((Saw) objectA).body.getType() != BodyType.StaticBody) {
+				if (objectA instanceof Saw && ((Saw) objectA).body.getType() != BodyType.StaticBody) {
 					splash = true;
 				}
-			} else if (objectB instanceof Obstacle || (objectB instanceof WreckingBall && ((WreckingBall)objectB).body.getType() != BodyType.StaticBody) || (objectB instanceof Saw && ((Saw)objectB).body.getType() != BodyType.StaticBody)) {
+			} else if (objectB instanceof Obstacle
+					|| (objectB instanceof WreckingBall
+							&& ((WreckingBall) objectB).body.getType() != BodyType.StaticBody)
+					|| (objectB instanceof Saw && ((Saw) objectB).body.getType() != BodyType.StaticBody)) {
 				if (((Player) objectA).getActive()) {
 					deadPlayer = ((Player) objectA);
 				}
-				if(objectB instanceof Saw && ((Saw) objectB).body.getType() != BodyType.StaticBody) {
+				if (objectB instanceof Saw && ((Saw) objectB).body.getType() != BodyType.StaticBody) {
 					splash = true;
 				}
 			}
@@ -106,24 +111,24 @@ public class ContactListener implements com.badlogic.gdx.physics.box2d.ContactLi
 	}
 
 	private void wreckingBall(final Object objectA, final Object objectB) {
-		if(objectA instanceof WreckingBall) {
-			if(objectB instanceof FreezingPlayer) {
+		if (objectA instanceof WreckingBall) {
+			if (objectB instanceof FreezingPlayer) {
 				Gdx.app.postRunnable(new Runnable() {
 
 					@Override
 					public void run() {
 						((WreckingBall) objectA).body.setType(BodyType.StaticBody);
 					}
-				});	
-			}
-			else if (objectB instanceof Player || objectB instanceof GroundSquare || objectB instanceof SpikeL || objectB instanceof BouncingBlock || objectB instanceof Ragdoll) {
-				if(((WreckingBall) objectA).body.getType() != BodyType.StaticBody) {
+				});
+			} else if (objectB instanceof Player || objectB instanceof GroundSquare || objectB instanceof SpikeL
+					|| objectB instanceof BouncingBlock || objectB instanceof Ragdoll) {
+				if (((WreckingBall) objectA).body.getType() != BodyType.StaticBody) {
 					Gdx.app.postRunnable(new Runnable() {
 						@Override
 						public void run() {
 							Vector2 spawn = ((WreckingBall) objectA).spawn;
 							World world = ((WreckingBall) objectA).world;
-							if(objectB instanceof BouncingBlock) {
+							if (objectB instanceof BouncingBlock) {
 								((BouncingBlock) objectB).destroy();
 							}
 							((WreckingBall) objectA).destroy();
@@ -133,26 +138,25 @@ public class ContactListener implements com.badlogic.gdx.physics.box2d.ContactLi
 					});
 				}
 			}
-		}
-		else if(objectB instanceof WreckingBall) {
-			if(objectA instanceof FreezingPlayer) {
+		} else if (objectB instanceof WreckingBall) {
+			if (objectA instanceof FreezingPlayer) {
 				Gdx.app.postRunnable(new Runnable() {
 
 					@Override
 					public void run() {
 						((WreckingBall) objectB).body.setType(BodyType.StaticBody);
 					}
-				});	
-			}
-			else if (objectA instanceof Player || objectA instanceof GroundSquare || objectA instanceof BouncingBlock || objectA instanceof Ragdoll) {
-				if(((WreckingBall) objectB).body.getType() != BodyType.StaticBody) {
+				});
+			} else if (objectA instanceof Player || objectA instanceof GroundSquare || objectA instanceof BouncingBlock
+					|| objectA instanceof Ragdoll) {
+				if (((WreckingBall) objectB).body.getType() != BodyType.StaticBody) {
 					Gdx.app.postRunnable(new Runnable() {
 
 						@Override
 						public void run() {
 							Vector2 spawn = ((WreckingBall) objectB).spawn;
 							World world = ((WreckingBall) objectB).world;
-							if(objectA instanceof BouncingBlock) {
+							if (objectA instanceof BouncingBlock) {
 								((BouncingBlock) objectA).destroy();
 							}
 							((WreckingBall) objectB).destroy();
@@ -163,28 +167,27 @@ public class ContactListener implements com.badlogic.gdx.physics.box2d.ContactLi
 			}
 		}
 	}
-	
+
 	private void saw(final Object objectA, final Object objectB) {
-		if(objectA instanceof Saw) {
-			if(objectB instanceof FreezingPlayer) {
+		if (objectA instanceof Saw) {
+			if (objectB instanceof FreezingPlayer) {
 				Gdx.app.postRunnable(new Runnable() {
 
 					@Override
 					public void run() {
 						((Saw) objectA).body.setType(BodyType.StaticBody);
 					}
-				});	
+				});
 			}
-		}
-		else if(objectB instanceof Saw) {
-			if(objectA instanceof FreezingPlayer) {
+		} else if (objectB instanceof Saw) {
+			if (objectA instanceof FreezingPlayer) {
 				Gdx.app.postRunnable(new Runnable() {
 
 					@Override
 					public void run() {
 						((Saw) objectB).body.setType(BodyType.StaticBody);
 					}
-				});	
+				});
 			}
 		}
 	}
