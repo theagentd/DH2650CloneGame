89029990a693fafb09b0da369884ad8f44f8973f
theagentd <theagentd@hotmail.com>
2017-12-05 21:37:54 +0100

    Added 8x antialiasing to greatly improve the look of the game.

--- CloneGame-desktop/src/com/clone/desktop/DesktopLauncher.java
+++ CloneGame-desktop/src/com/clone/desktop/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
+		config.samples = 8;
 		new LwjglApplication(new CloneGame(), config);
 	}
 }
