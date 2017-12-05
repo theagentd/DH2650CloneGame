035715a6e23c719e4a639323fa7d2c3aa2ec1e49
theagentd <theagentd@hotmail.com>
2017-12-05 21:29:22 +0100

    Fixed end flag.

--- CloneGame-core/src/com/clone/End.java
+++ CloneGame-core/src/com/clone/End.java
@@ -1,32 +1,46 @@
 package com.clone;
 
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.clone.fixture3d.BoxFixture3D;
+import com.clone.fixture3d.Fixture3D;
 import com.badlogic.gdx.physics.box2d.EdgeShape;
 
 public class End {
-	
-	protected BodyDef bodyDef;
-	protected Body body;
-	protected Vector2[] points;
+
 	protected World world;
 	
-	public End(World world, Vector2 ... points) {
+	protected Body flagBody;
+	protected Fixture3D flagFixture;
+	
+	protected Body poleBody;
+	protected Fixture3D poleFixture;
+	
+	
+	public End(World world, Vector2 position) {
 		this.world = world;
-		this.points = points;
-		bodyDef = new BodyDef();
+		
+		BodyDef bodyDef = new BodyDef();
 		bodyDef.type = BodyType.StaticBody;
 		
-		bodyDef.position.set(points[0]);
-		FixtureDef fixtureSquare = new FixtureDef();
+		bodyDef.position.set(position);
+		flagBody = world.createBody(bodyDef);
+		
+		bodyDef.position.set(position).add(-6, -8);
+		poleBody = world.createBody(bodyDef);
+		
+		flagFixture = new BoxFixture3D(flagBody, 1, 0.1f, 0.1f, 6, 4, 0.1f, 0, 1, 0.1f, 0.1f, (short)0, this);
+		
+		poleFixture = new BoxFixture3D(poleBody, 1, 1, 1, 0.15f, 12, 0.15f, 0, 1, 0.1f, 0.1f, (short)0, this);
+		
+		
+		
+		/*FixtureDef fixtureSquare = new FixtureDef();
 		//Shape Flag (rectangle)
 		PolygonShape rectangle = new PolygonShape();
 		rectangle.setAsBox(6f, 4f);
@@ -35,8 +49,7 @@ public class End {
 		fixtureSquare.friction = 0.1f;
 		fixtureSquare.restitution = 0f;
 		
-		body = world.createBody(bodyDef);
-		body.createFixture(fixtureSquare).setUserData(this);
+		flagBody.createFixture(fixtureSquare).setUserData(this);
 		// shape Flag (line)
 		EdgeShape line = new  EdgeShape();
 		line.set(-6, 0, -6, -20);
@@ -47,12 +60,7 @@ public class End {
 		fixtureSquare.friction = 0.1f;
 		fixtureSquare.restitution = 0f;
 		
-		body.createFixture(fixtureLine).setUserData(this);
+		flagBody.createFixture(fixtureLine).setUserData(this);*/
 		
 	}
-	
-	public void dispose() {
-	}
-	
-	
 }
