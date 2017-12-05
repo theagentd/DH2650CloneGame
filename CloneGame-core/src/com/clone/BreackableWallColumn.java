f63308a222cc5cc913ecac89e13bfa076950b2d0
theagentd <theagentd@hotmail.com>
2017-12-05 18:43:14 +0100

    Very minor clean up.

--- CloneGame-core/src/com/clone/BreackableWallColumn.java
+++ CloneGame-core/src/com/clone/BreackableWallColumn.java
@@ -7,10 +7,8 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.clone.fixture3d.BoxFixture3D;
 
 public class BreackableWallColumn extends Breakable {
-	Fixture fix;
 	public BreackableWallColumn(World world, Vector2 points) {
 		super(world, points);
-		
 		addFixture(new BoxFixture3D(body, 0.25f, 0.5f, 0.25f, 30f * GroundSquare.SCALE, 90f * GroundSquare.SCALE, 20 * GroundSquare.SCALE, 0, 2.5f, 0.1f, 0.01f, (short)0, this));
 	}
 }
